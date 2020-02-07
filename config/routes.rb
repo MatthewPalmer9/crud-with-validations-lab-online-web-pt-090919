8ce3a9d55c653e128753c794117bf1c63f8a8cf2
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-07 00:49:46 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :songs
-<<<<<<< HEAD
   get '/', to: 'songs#index'
-=======
->>>>>>> b1f6ee21f36d368e899a27af443556783356135a
 end
